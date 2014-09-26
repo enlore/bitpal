4b2f8aa4ba6f96077bc099bc1052ffb817951cfb
enlore <n.e.lorenson@gmail.com>
2014-09-26 15:16:57 -0500

    Form sub handler

--- app/index.js
+++ app/index.js
@@ -3,15 +3,21 @@ var express = require('express')
 ;
 
 var path    = require('path')
+    , bodyParser    = require('body-parser')
+    ;
 
 app.set('port', process.env.PORT || 3000)
 
 var staticPath = path.join(__dirname, '../public')
 app.use(express.static(staticPath))
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.post('/signup', function (req, res) {
-    if (app.get('env') === 'development')
+    if (app.get('env') === 'development') {
         console.log('~~~~~> Form sub')
+        console.log(req.body)
+    }
 
     res.redirect('/')
 })
