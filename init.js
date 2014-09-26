746ccddfa93a5133d6a3834f95093ad7729c74ec
enlore <n.e.lorenson@gmail.com>
2014-09-26 18:53:01 -0500

    Export function, require string error

--- init.js
+++ init.js
@@ -1,6 +1,8 @@
-var db = require('db')
+var db = require('./db')
 
-db.serialize(function () {
-    db.run('DROP TABLE IF EXISTS emails')
-    db.run('CREATE TABLE emails (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT)')
-})
+module.exports = function () {
+    db.serialize(function () {
+        db.run('DROP TABLE IF EXISTS emails')
+        db.run('CREATE TABLE emails (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT)')
+    })
+}
