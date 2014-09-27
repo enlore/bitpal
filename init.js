59cf3de46c476df3c43ddf0131234401449758be
enlore <n.e.lorenson@gmail.com>
2014-09-26 23:56:44 -0500

    Add column to dev db

--- init.js
+++ init.js
@@ -3,6 +3,6 @@ var db = require('./db')
 module.exports = function () {
     db.serialize(function () {
         db.run('DROP TABLE IF EXISTS emails')
-        db.run('CREATE TABLE emails (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT)')
+        db.run('CREATE TABLE emails (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, created NUMBER)')
     })
 }
